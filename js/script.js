105ff91ebd76016ebdf53a530af2319473a0e56d
RealMeKaNiK <dautovic2000@gmail.com>
2022-02-04 22:21:04 +0100

    modal

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const modal = document.querySelector(".contact-modal");
+const openButton = document.querySelectorAll(".open-button");
+const closeButton = document.querySelector(".cancel-button");
+
 //Toggle mobile mode
 hamburger.addEventListener("click", (e) => {
   mobileMenu.classList.toggle("hidden");
 });
+
+//Modal open and close events
+openButton.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    modal.classList.toggle("hidden");
+  });
+});
+
+closeButton.addEventListener("click", (e) => {
+  modal.classList.toggle("hidden");
+});
+
+window.onclick = (e) => {
+  if (e.target == modal) {
+    modal.classList.toggle("hidden");
+  }
+};
