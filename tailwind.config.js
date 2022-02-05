433e93ad4078ba7b5e295fb9746ec40bd5016d4b
RealMeKaNiK <dautovic2000@gmail.com>
2022-02-05 10:53:19 +0100

    news

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ["./*.{html, js}", "./*/*.{html, js}"],
   theme: {
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "15px",
+    },
     extend: {},
   },
   plugins: [],
