444ed0a14e73b0b5bf90968f90a17741c0120a78
RealMeKaNiK <dautovic2000@gmail.com>
2022-02-06 23:30:51 +0100

    animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,11 +20,16 @@ module.exports = {
       },
       animation: {
         flyIn: "flyIn 2s ease-in-out",
+        flyInAbove: "flyInAbove 1s ease-in-out",
       },
       keyframes: {
         flyIn: {
           "0%": { transform: "translateX(-200%)" },
           "100%": { transform: "translateX(0)" },
+        },
+        flyInAbove: {
+          "0%": { transform: "translateY(-200%)" },
+          "100%": { transform: "translateY(0)" },
         },
       },
     },
